d5f7ff3b34df1879a6818b57391734789d4e2ed6
GRMMTS18 <egraim18@gmail.com>
2025-11-21 15:44:25 -0300

    Create database and produtos table

---
+++ sql.sql
@@ -0,0 +1,11 @@
+CREATE DATABASE catalogo_db;
+
+CREATE TABLE produtos (
+    id INT(11) UNSIGNED AUTO_INCREMENT PRIMARY KEY,
+    nome VARCHAR(255) NOT NULL,
+    descricao TEXT,
+    preco DECIMAL(10, 2) NOT NULL,
+    categoria VARCHAR(50) NOT NULL,
+    imagem_url VARCHAR(512),
+    data_cadastro TIMESTAMP DEFAULT CURRENT_TIMESTAMP
+);
